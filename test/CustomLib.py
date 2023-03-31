1fc54ed2fed1a9b1629842ca8559e46eb97b505d
Siddhartha Patki <siddharthapatki@gmail.com>
2023-03-31 17:12:58 +0530

    removed geoloc and local

--- test/CustomLib.py
+++ test/CustomLib.py
@@ -7,19 +7,15 @@ import os
 
 class CustomLib:
     desired_cap = {
-    'os': 'osx',
-    'os_version': 'catalina',
-    'browser': 'playwright-firefox',  # allowed browsers are `chrome`, `edge`, `playwright-chromium`, `playwright-firefox` and `playwright-webkit`
-    # 'browser_version': 'latest', # this capability is valid only for branded `chrome` and `edge` browsers and you can specify any browser version like `latest`, `latest-beta`, `latest-1` and so on.
+    'os': 'os x',
+    'os_version': 'Ventura',
+    'browser': 'playwright-webkit',  # allowed browsers are `chrome`, `edge`, `playwright-chromium`, `playwright-firefox` and `playwright-webkit`
     'browserstack.username': os.environ['BROWSERSTACK_USERNAME'],
     'browserstack.accessKey': os.environ['BROWSERSTACK_ACCESS_KEY'],
-    'browserstack.geoLocation': 'FR',
     'project': 'BStack Project',
-    'build': 'browserstack-build-1',
+    'build': 'browserstack-build-11',
     'buildTag': 'Regression',
     'resolution': '1280x1024',
-    'browserstack.local': 'true',
-    'browserstack.localIdentifier': 'local_connection_name',
     'browserstack.playwrightVersion': '1.latest',
     'client.playwrightVersion': '1.latest',
     'browserstack.debug': 'true',  # enabling visual logs
@@ -33,8 +29,8 @@ class CustomLib:
 
 
     def createCdpUrl(self):
-        clientPlaywrightVersion = str(subprocess.getoutput('playwright --version')).strip().split(" ")[1]
-        CustomLib.desired_cap['client.playwrightVersion'] = clientPlaywrightVersion
+        # clientPlaywrightVersion = str(subprocess.getoutput('playwright --version')).strip().split(" ")[1]
+        # CustomLib.desired_cap['client.playwrightVersion'] = clientPlaywrightVersion
         cdpUrl = 'wss://cdp.browserstack.com/playwright?caps=' + urllib.parse.quote(json.dumps(CustomLib.desired_cap))
         print(cdpUrl)
         return cdpUrl
